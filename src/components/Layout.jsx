6769528c8ff40db6b120541943bf17fa58e73b7c
Celestine Jepkemboi <celestinejepkemboi4@gmail.com>
2025-10-22 10:17:53 +0300

    Create Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 max-w-6xl mx-auto px-4 py-6 w-full">{children}</main>
+      <Footer />
+    </div>
+  )
+}
